dab1774b2a118cedc64962800fa953c5041e9690
ibrahim wais <67577024+Ibrahim-wais-22@users.noreply.github.com>
2022-09-19 09:08:16 -0700

    end edit

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,21 @@ document.querySelector(".timeS").innerHTML= time.getSeconds();
 document.querySelector(".timeD").innerHTML= time.getDate();
 document.querySelector(".timeM").innerHTML= time.getMonth();
 document.querySelector(".timeY").innerHTML= time.getFullYear();
+
+// ----------------------------------------------------------------
+function random1(){
+
+    document.querySelector("#showNumber").innerHTML= Math.random()*100;
+}
+function floor1(){
+    let f = document.getElementById('showNumber').textContent;
+    document.querySelector("#showNumber").innerHTML= Math.floor(f);
+}
+function round1(){
+    let f = document.getElementById('showNumber').textContent;
+    document.querySelector("#showNumber").innerHTML= Math.round(f);
+}
+function cell1(){
+    let f = document.getElementById('showNumber').textContent;
+    document.querySelector("#showNumber").innerHTML= Math.ceil(f);
+}
